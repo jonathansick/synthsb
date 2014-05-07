24fc187ad7230b48a018cbcbe9d245d18483ae1d
Jonathan Sick <jonathansick@mac.com>
2014-05-07 18:15:54 -0400

    deal with weightmaps that don't exist

--- hstsb/phatsb.py
+++ hstsb/phatsb.py
@@ -64,10 +64,13 @@ def process_acs_field(brick, fieldnum):
     fieldname = "phat_b{0:02d}_f{1:02d}".format(brick, fieldnum)
     data = load_photometry(fieldname, 'phat_acs', 'f475w', 'f814w')
 
-    image_path = phat_field_path(brick, fieldnum, "f814w")
-    header = fits.getheader(image_path, 'SCI')
-    whtdata = fits.getdata(image_path, 'WHT')
-    A = compute_area_from_weights(whtdata, header)
+    try:
+        A, header = compute_area("f475w", brick, fieldnum)
+    except NoWeightmap:
+        try:
+            A, header = compute_area("f814w", brick, fieldnum)
+        except NoWeightmap:
+            raise NoDataError
     
     sb475, e475 = compute_sb(data['m1'], data['e1'], data['cfrac'], A)
     sb814, e814 = compute_sb(data['m2'], data['e2'], data['cfrac'], A)
@@ -108,10 +111,13 @@ def process_ir_field(brick, fieldnum):
     fieldname = "phat_b{0:02d}_f{1:02d}".format(brick, fieldnum)
     data = load_photometry(fieldname, 'phat_ir', 'f110w', 'f160w')
 
-    image_path = phat_field_path(brick, fieldnum, "f110w")
-    header = fits.getheader(image_path, 'SCI')
-    whtdata = fits.getdata(image_path, 'WHT')
-    A = compute_area_from_weights(whtdata, header)
+    try:
+        A, header = compute_area("f110w", brick, fieldnum)
+    except NoWeightmap:
+        try:
+            A, header = compute_area("f160w", brick, fieldnum)
+        except NoWeightmap:
+            raise NoDataError
     
     sb110, e110 = compute_sb(data['m1'], data['e1'], data['cfrac'], A)
     sb160, e160 = compute_sb(data['m2'], data['e2'], data['cfrac'], A)
@@ -147,11 +153,13 @@ def process_uv_field(brick, fieldnum):
     """Compute UV SB in this brick+field."""
     fieldname = "phat_b{0:02d}_f{1:02d}".format(brick, fieldnum)
     data = load_photometry(fieldname, 'phat_uv', 'f275w', 'f336w')
-
-    image_path = phat_field_path(brick, fieldnum, "f275w")
-    header = fits.getheader(image_path, 'SCI')
-    whtdata = fits.getdata(image_path, 'WHT')
-    A = compute_area_from_weights(whtdata, header)
+    try:
+        A, header = compute_area("f275w", brick, fieldnum)
+    except NoWeightmap:
+        try:
+            A, header = compute_area("f336w", brick, fieldnum)
+        except NoWeightmap:
+            raise NoDataError
     
     sb275, e275 = compute_sb(data['m1'], data['e1'], data['cfrac'], A)
     sb336, e336 = compute_sb(data['m2'], data['e2'], data['cfrac'], A)
@@ -160,6 +168,21 @@ def process_uv_field(brick, fieldnum):
             "ra": ra0, "dec": dec0, "radius": rad.kpc,
             "f275w": sb275, "f336w": sb336,
             "f275w_err": e275, "f336w_err": e336}
+
+
+class NoWeightmap(BaseException):
+    pass
+
+
+def compute_area(band, brick, fieldnum):
+    image_path = phat_field_path(brick, fieldnum, band)
+    header = fits.getheader(image_path, 'SCI')
+    try:
+        whtdata = fits.getdata(image_path, 'WHT')
+    except:
+        raise NoWeightmap
+    A = compute_area_from_weights(whtdata, header)
+    return A, header
 
 
 if __name__ == '__main__':
